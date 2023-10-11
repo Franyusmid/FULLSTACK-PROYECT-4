3c9fd3cab57a751ff15955cd7537f18ce0456824
Franyusd Gomez Bolivar <analistacost@gmail.com>
2023-10-10 20:06:58 -0500

    se conecta el repositorio con RENDER y se verifican el funcionamiento de GET y POST

--- server/index.js
+++ server/index.js
@@ -11,19 +11,36 @@ const data=[{
     title:"reservación 1",
     descripcion:"quiero una reservacion a colombia"
 },   {
-      id:0,
+      id:1,
      title:"reservación 1",
       descripcion:"quiero una reservacion a colombia"
  }  ]
 
 //3.RUTAS
+//GET-OBTENCION DE DATOS (RESERVACIONES)
 //localhost:3005/
+
 
 app.get("/",(req,res)=>{
 
     res.json({
         msg:"Este es un menssaje",
         data:data
+    })
+})
+
+// POST-CREA UNA RESERVACION 
+app.post("/",(req,res)=>{
+
+    data.push({
+        id:2,
+        title:"reservacion 2",
+        descripcion:"quiero una reservacion de mesa para 2 personas"
+    })
+
+    res.json({
+        msg:"reservacion agregada",
+        data:data,
     })
 })
 
