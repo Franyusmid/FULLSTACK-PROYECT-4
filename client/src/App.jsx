6d9dc3b8de4c29b59a82b7f157652b49a489529f
Franyusmid <121640093+Franyusmid@users.noreply.github.com>
2023-11-07 18:29:31 -0500

    se organiza la imagen del logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 
 import Layout from './components/layout';
 import Reservation from './components/reservation';
+import lasagna from './assets/imagenes/lasagna.png';
+
 
 import AboutUs from './components/aboutUs';
 
@@ -13,7 +15,7 @@ function App() {
       <div className='flex flex-col min-h-screen'>
         <div className='bg-[#a8a47f] flex flex-row h-32'>
           <div className='basis-1/4 p-2 '>
-            <img src="https://photos.google.com/photo/AF1QipOTtmS7sSGQuXcO8sNP9ppuqpRu7aC6T5Fwqa1L"
+            <img src={lasagna}
              className='h-28 w-28' />
           </div>
           <div className='grow w-full h-full flex justify-evenly items-center'>
--- client/src/assets/img.jsx
+++ client/src/assets/img.jsx
@@ -1,7 +1,7 @@
 import hamburguesa from './imagenes/hamburguesa.png';
 import lasagna from './imagenes/lasagna.png';
-import logo from './imagenes/logo.png';
-import pizza from './imagenes/pizza.png';
+// import logo from './imagenes/logo.png';
+// import pizza from './imagenes/pizza.png';
 import vino from './imagenes/vino.png';
 
-export { hamburguesa, lasagna, logo, pizza, vino };
+export default { hamburguesa, lasagna, vino };
